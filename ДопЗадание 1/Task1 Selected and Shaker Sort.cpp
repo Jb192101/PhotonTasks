0b58d6d4332f5e4fee4853172aef8133fdefe4ce
Jb192101 <grigorijbaryshev910@gmail.com>
2025-06-11 08:47:19 +0300

    Доп.задания

---
+++ ДопЗадание 1/Task1 Selected and Shaker Sort.cpp
@@ -0,0 +1,151 @@
+﻿// Task1 Quick and Shaker Sort.cpp
+// Сравнение времени работы сортировки выбором и шейкер сортировки
+// Дата выполнения: 3 декабря 2024
+
+// Библиотеки
+#include <iostream>
+#include <vector>
+#include <chrono>
+
+// Генерация рандомных значений
+std::vector<__int64> rand_generation(std::vector<__int64> massive, int N)
+{
+    srand(time(NULL));
+    for (int i = 0; i < N; i++)
+    {
+        massive[i] = rand() % _I64_MAX;
+    }
+
+    return massive;
+}
+
+// Сортировка выбором O(N^2)
+std::chrono::duration<double> selected_sort(std::vector<__int64> massive, int N)
+{
+    int min; // для индекса
+
+    int l = 0;
+    int r = N - 1;
+
+    auto t1 = std::chrono::high_resolution_clock::now();
+    for (int i = l; i < r; i++)
+    {
+        int min = i;
+        for (int j = i + 1; j <= r; j++)
+            if (massive[j] < massive[min])
+                min = j;
+        std::swap(massive[i], massive[min]);
+    }
+    auto t2 = std::chrono::high_resolution_clock::now();
+
+    std::cout << "Быстрая сортировка : " << std::endl;
+    for (int i = 0; i < N; i++)
+    {
+        std::cout << massive[i] << " ";
+    }
+    std::cout << std::endl;
+    std::cout << std::endl;
+
+    std::chrono::duration<double> timeExecution = t2 - t1;
+
+    return timeExecution;
+}
+
+// Шейкер сортировка O(N*log(N))
+std::chrono::duration<double> shaker_sort(std::vector<__int64> massive, int N)
+{
+    int left = 0;
+    int right = N - 1;
+
+    auto t1 = std::chrono::high_resolution_clock::now();
+    do
+    {
+        for (int i = right; i > left; i--)
+        {
+            if (massive[i] < massive[i - 1])
+            {
+                std::swap(massive[i], massive[i - 1]);
+            }
+        }
+
+        left++;
+
+        for (int i = left; i < right; i++)
+        {
+            if (massive[i] > massive[i + 1])
+            {
+                std::swap(massive[i], massive[i + 1]);
+            }
+        }
+
+        right--;
+    } while (left <= right);
+
+    auto t2 = std::chrono::high_resolution_clock::now();
+
+    std::cout << "Шейкер сортировка : " << std::endl;
+    for (int i = 0; i < N; i++)
+    {
+        std::cout << massive[i] << " ";
+    }
+    std::cout << std::endl;
+    std::cout << std::endl;
+
+    std::chrono::duration<double> timeExecution = t2 - t1;
+
+    return timeExecution;
+}
+
+int main(int argc, char** argv)
+{
+    // Предполагается, что данные будут получены через параметры запуска
+    // argv[0] - путь
+    // argv[1] - значение N
+    setlocale(LC_ALL, "RU");
+
+    int N;
+    std::chrono::duration<double> time_select;
+    std::chrono::duration<double> time_shaker;
+    
+    //N = (int) argv[1];
+    std::cin >> N;
+
+    if (N <= 0)
+    {
+        std::cout << "Ошибка : некорректный размер массива!" << std::endl;
+        return 1;
+    }
+
+    std::vector<__int64> massive(N);
+
+    massive = rand_generation(massive, N);
+
+    std::cout << "Исходный массив : " << std::endl;
+    for (int i = 0; i < N; i++)
+    {
+        std::cout << massive[i] << " ";
+    }
+    std::cout << std::endl;
+
+    time_select = selected_sort(massive, N);
+    time_shaker = shaker_sort(massive, N);
+
+    std::cout << "Время работы сортировок : " << std::endl;
+    std::cout << "Сортировка выбором (Selected Sort) : " << time_select.count() * 100000 << " миллисекунд(ы)" << std::endl;
+    std::cout << "Шейкер сортировка (Shaker Sort) : " << time_shaker.count() * 100000 << " миллисекунд(ы)" << std::endl;
+
+    if (time_select < time_shaker)
+    {
+        std::cout << "Сортировка выбором работает за меньшее время" << std::endl;
+    }
+    else if (time_select > time_shaker)
+    {
+        std::cout << "Шейкер сортировка работает за меньшее время" << std::endl;
+    }
+    else
+    {
+        std::cout << "Обе сортировки работают за одинаковое время" << std::endl;
+    }
+
+    return 0;
+}
---
+++ ДопЗадание 2/Task 2 Exchange bits.cpp
@@ -0,0 +1,57 @@
+﻿// Task 2 Exchange bits.cpp
+// Дата выполнения: 3 декабря 2024
+
+// ПОЯСНИТЕЛЬНЫЕ МОМЕНТЫ:
+// ~ - побитовая инверсия
+// >>, << - побитовый сдвиг вправо и влево (слева направо и наоборот)
+// & - побитовое И
+// | - побитовое ИЛИ
+// ^ - побитовое исключащее ИЛИ
+
+// В числе x младшие m бит заменить на старшие m бит из y, потом инвертировать младшие n бит в числе y
+#include <iostream>
+
+std::string toBinary(__int64 n) {
+    if (n == 0) return "0";
+
+    std::string binaryString = "";
+    while (n > 0) {
+        binaryString = (n % 2 == 0 ? "0" : "1") + binaryString;
+        n /= 2;
+    }
+    return binaryString;
+}
+
+int main() {
+    setlocale(LC_ALL, "RU");
+
+    __int64 x, y, m, n;
+    std::cin >> x >> y >> m >> n;
+
+    std::cout << "Исходные данные:" << std::endl;
+    std::cout << "x = " << x << std::endl;
+    std::cout << "y = " << y << std::endl;
+    std::cout << "m = " << m << std::endl;
+    std::cout << "n = " << n << std::endl;
+
+
+    // Замена младших m бит в x на старшие m бит из y
+    __int64 mask_x = (~((1LL << m) - 1));
+    __int64 mask_y = ((1LL << m) - 1) << (64 - m);
+
+    x = (x & mask_x) | ((y & mask_y) >> (64 - m));
+
+
+    // Инвертирование n младших бит в y
+    y ^= ((1LL << n) - 1);
+
+    std::cout << "\nИзмененные данные:" << std::endl;
+    std::cout << "-----------------------------------------" << std::endl;
+    std::cout << "| Переменная |  Двоичное представление  |" << std::endl;
+    std::cout << "-----------------------------------------" << std::endl;
+    std::cout << "|     x      |     " << toBinary(x) << "          |" << std::endl;
+    std::cout << "|     y      |     " << toBinary(y) << "         |" << std::endl;
+    std::cout << "-----------------------------------------" << std::endl;
+
+    return 0;
+}
